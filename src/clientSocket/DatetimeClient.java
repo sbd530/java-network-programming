c504bb4a546f921d8ffc1f53b56c2b112370fb65
don <byungdon92@gmail.com>
2021-09-16 22:58:54 +0900

    Client Socket

--- src/clientSocket/DatetimeClient.java
+++ src/clientSocket/DatetimeClient.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 
 public class DatetimeClient {
